4d46d10685f3f20951d71578b88d74a7160b8ee8
Maru <cesarmvillalobos@hotmail.com>
2024-04-21 20:57:37 -0700

    Se corrigio un error que no permitia que signin reconociera una cuenta en la DB

--- main/views.py
+++ main/views.py
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
 from django.shortcuts import render,redirect
 from django.contrib.auth import login,logout,authenticate
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
-=======
-from django.shortcuts import render
-
->>>>>>> f3eb7c58c0a32e862e79d0c5ad3b78e85c0fa09d
 # Create your views here.
 def signup(request):
     if request.method == 'GET':
@@ -34,20 +29,20 @@ def signup(request):
 
 
 def signin(request):
-<<<<<<< HEAD
+
     if request.method =='GET':    
         return render(request,'signin.html',{
             'form': AuthenticationForm
         })
     else:
-        user = authenticate(request,username=request.POST['username'],password=request.POST['password'])
+        user = authenticate(request,username=request.POST['usuario'],password=request.POST['password'])
         if user is None:
             return render(request,'signin.html',{
                 'form': AuthenticationForm,
                 'error': 'Usuario o contraseña incorrecta'
             })
         else:
-            #login(request,user)
+            login(request,user)
             return redirect('inicio')
 
 def inicio(request):
@@ -56,6 +51,3 @@ def inicio(request):
 def signout(request):
     logout(request)
     return redirect('signin')
-=======
-    return render(request,'signin.html')
->>>>>>> f3eb7c58c0a32e862e79d0c5ad3b78e85c0fa09d
