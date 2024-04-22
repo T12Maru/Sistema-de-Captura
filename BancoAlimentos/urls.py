e003d17db5ca1b77a4da915afc4a72ceb742a327
Maru <cesarmvillalobos@hotmail.com>
2024-04-21 20:08:37 -0700

    La url /signup ahora permite agregar usuarios a la DB e iniciar/cerrar una sesion

--- BancoAlimentos/urls.py
+++ BancoAlimentos/urls.py
@@ -19,5 +19,7 @@ from main import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('signup/',views.signup,name='signup'),
-    path('',views.signin,name='signin')
+    path('signin/',views.signin,name='signin'),
+    path('logout/',views.signout,name = 'logout'),
+    path('',views.inicio,name='inicio')
 ]
--- main/views.py
+++ main/views.py
@@ -1,9 +1,53 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth import login,logout,authenticate
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
 
 # Create your views here.
 def signup(request):
-    return render(request,'signup.html')
+    if request.method == 'GET':
+        return render(request,'signup.html',{
+            'form': UserCreationForm
+        })
+    else:
+        if request.POST['password'] == request.POST['confirmP']:
+            try:
+                #registrando un usuario
+                user = User.objects.create_user(username=request.POST['usuario'],password=request.POST['password'])
+                user.save()
+                login(request,user)
+                return redirect('inicio')
+            except IntegrityError:
+                return render(request,'signup.html',{
+                    'form': UserCreationForm,
+                    'error': 'El usuario ya existe'
+                })
+        return render(request,'signup.html',{
+            'form': UserCreationForm,
+            'error': 'Las contraseñas no coinciden'
+        })
+
 
 def signin(request):
-    return render(request,'signin.html')
+    if request.method =='GET':    
+        return render(request,'signin.html',{
+            'form': AuthenticationForm
+        })
+    else:
+        user = authenticate(request,username=request.POST['username'],password=request.POST['password'])
+        if user is None:
+            return render(request,'signin.html',{
+                'form': AuthenticationForm,
+                'error': 'Usuario o contraseña incorrecta'
+            })
+        else:
+            #login(request,user)
+            return redirect('inicio')
+
+def inicio(request):
+    return render(request,'inicio.html')
+
+def signout(request):
+    logout(request)
+    return redirect('signin')
