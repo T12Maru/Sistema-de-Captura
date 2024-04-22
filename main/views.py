6ad4d1b1990af880a7c46c479f904bb8109ca843
Maru <cesarmvillalobos@hotmail.com>
2024-04-21 20:28:18 -0700

    Commit del profesor

--- main/views.py
+++ main/views.py
@@ -1,8 +1,12 @@
+<<<<<<< HEAD
 from django.shortcuts import render,redirect
 from django.contrib.auth import login,logout,authenticate
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
+=======
+from django.shortcuts import render
+>>>>>>> f3eb7c58c0a32e862e79d0c5ad3b78e85c0fa09d
 
 # Create your views here.
 def signup(request):
@@ -30,6 +34,7 @@ def signup(request):
 
 
 def signin(request):
+<<<<<<< HEAD
     if request.method =='GET':    
         return render(request,'signin.html',{
             'form': AuthenticationForm
@@ -51,3 +56,6 @@ def inicio(request):
 def signout(request):
     logout(request)
     return redirect('signin')
+=======
+    return render(request,'signin.html')
+>>>>>>> f3eb7c58c0a32e862e79d0c5ad3b78e85c0fa09d
